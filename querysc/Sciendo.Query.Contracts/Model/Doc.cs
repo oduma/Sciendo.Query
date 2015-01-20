47c8742ceb10e9f27ed4c31a4d727bac8ad92ed5
oduma <tavi_duma2002@yahoo.co.uk>
2015-01-20 19:28:41 +0000

    added solr Contracts

---
+++ querysc/Sciendo.Query.Contracts/Model/Doc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sciendo.Query.Contracts.Model
+{
+    public class Doc
+    {
+        public string filePath { get; set; }
+        public string artist { get; set; }
+        public string album { get; set; }
+        public string title { get; set; }
+        public string lyrics { get; set; }
+
+    }
+}
---
+++ querysc/Sciendo.Query.Contracts/Model/Response.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Sciendo.Query.Contracts.Model
+{
+    public class Response
+    {
+        public int numFound { get; set; }
+
+        public int start { get; set; }
+
+        public Doc[] docs { get; set; }
+    }
+}
