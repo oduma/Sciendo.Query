cb227892f180472e0c8ed9b1084e01bd5b9c8122
oduma <tavi_duma2002@yahoo.co.uk>
2015-01-20 23:29:53 +0000

    json file commited

--- querysc/Sciendo.Query.DataProviders/Solr/SolRetriever.cs
+++ querysc/Sciendo.Query.DataProviders/Solr/SolRetriever.cs
@@ -1,35 +1,30 @@
-﻿using System;
+﻿using Sciendo.Query.Contracts.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace Sciendo.Query.DataProviders.Solr
 {
     public static class SolRetriever
     {
-        //public static TrySendResponse TryQuery<T>(string url, string query, SolrQueryStrategy solrQueryStrategy)
-        //{
-        //    HttpClient httpClient = new HttpClient();
-        //    using (var postTask = httpClient.PostAsJsonAsync<T>(new Uri(url), package)
-        //        .ContinueWith((p) => p).Result)
-        //    {
-        //        if (postTask.Status != TaskStatus.RanToCompletion || !postTask.Result.IsSuccessStatusCode || postTask.Result.Content == null)
-        //            return new TrySendResponse { Status = Status.NotIndexed };
+        public static SolrResponse TryQuery(string url, string query, SolrQueryStrategy solrQueryStrategy)
+        {
+            HttpClient httpClient = new HttpClient();
+            using (var getTask = httpClient.GetStringAsync(new Uri(url+"?" +solrQueryStrategy))
+                .ContinueWith((p) => p).Result)
+            {
+                if (getTask.Status == TaskStatus.RanToCompletion || !string.IsNullOrEmpty(getTask.Result))
+                {
+                    return JsonConvert.DeserializeObject<SolrResponse>(getTask.Result);
+                }
+                return null;
+            }
 
-        //        using (var readTask = postTask.Result.Content.ReadAsStringAsync().ContinueWith((r) => r).Result)
-        //        {
-        //            if (readTask.Status != TaskStatus.RanToCompletion || string.IsNullOrEmpty(readTask.Result))
-        //                return new TrySendResponse { Status = Status.NotIndexed };
-        //            var solrUpdateResponse = JsonConvert.DeserializeObject<SolrUpdateResponse>(readTask.Result);
-        //            if (solrUpdateResponse.responseHeader.status != 0)
-        //                return new TrySendResponse { Status = Status.NotIndexed, Time = solrUpdateResponse.responseHeader.QTime };
-        //            return new TrySendResponse { Status = Status.Done, Time = solrUpdateResponse.responseHeader.QTime };
-
-        //        }
-        //    }
-
-        //}
+        }
 
     }
 }
